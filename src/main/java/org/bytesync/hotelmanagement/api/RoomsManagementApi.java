f9e0eb5a1b8dd7ab6d359cfec6223c3ed9c11961
PyaePhyoL <pyaephyolwin655@gmail.com>
2025-10-14 11:17:50 +0630

    Added cross origin in Guest and room apis

--- src/main/java/org/bytesync/hotelmanagement/api/RoomsManagementApi.java
+++ src/main/java/org/bytesync/hotelmanagement/api/RoomsManagementApi.java
@@ -1,9 +1,16 @@
 package org.bytesync.hotelmanagement.api;
 
+import lombok.RequiredArgsConstructor;
+import org.bytesync.hotelmanagement.service.RoomService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
-@RequestMapping
+@RequestMapping("/rooms/api")
+@RequiredArgsConstructor
 public class RoomsManagementApi {
+
+    private final RoomService roomService;
 }
